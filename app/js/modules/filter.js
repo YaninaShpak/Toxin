bcfed1affa2d91501eef0458e9126a5faf8ccdc8
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2022-06-06 09:57:48 +0300

    Доработала страницу каталога на мобильной версии
    Изменила поведение блоков фильтр и каталог.
    При открытии фильтров добавила кнопки применить, сбросить и закрыть

--- app/js/modules/filter.js
+++ app/js/modules/filter.js
@@ -1,10 +1,12 @@
 let btnOpenFilter = document.querySelector('.catalog-content__button');
 let btnCloseFilter = document.querySelector('.filter__button--close');
 let filter = document.querySelector('.filter');
+let catalog = document.querySelector('.catalog');
 
 export const showFilter = function () {
   btnOpenFilter.addEventListener('click', () => {
     filter.classList.remove('hidden');
+    catalog.classList.add('hidden');
     btnOpenFilter.classList.add('hidden');
   })
 };
@@ -12,6 +14,7 @@ export const showFilter = function () {
 export const closeFilter = function () {
   btnCloseFilter.addEventListener('click', () => {
     filter.classList.add('hidden');
+    catalog.classList.remove('hidden');
     btnOpenFilter.classList.remove('hidden');
   })
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+Ссылка на проект: https://yaninashpak.github.io/Toxin/index.html
+
+Макет: https://www.figma.com/file/MumYcKVk9RkKZEG6dR5E3A/MetaLamp-(former-FSD)-frontend-education-program.-The-2nd-task?node-id=0%3A1
+
+**Проект будет дорабатываться
